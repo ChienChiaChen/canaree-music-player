1ca01a72edfed63280d8027cda4d4a2810f62b79
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-05-20 02:35:48 +0200

    blocked strange lastfm behavior

--- image-provider/src/main/java/dev/olog/msc/imageprovider/glide/fetcher/GlideArtistFetcher.kt
+++ image-provider/src/main/java/dev/olog/msc/imageprovider/glide/fetcher/GlideArtistFetcher.kt
@@ -8,6 +8,10 @@ import dev.olog.msc.core.PrefsKeys
 import dev.olog.msc.core.gateway.LastFmGateway
 import java.io.InputStream
 
+// for some reason last fm for some artists (maybe all) is returning a start instead of the artist image, this
+// is the name of the image
+private const val LAST_FM_PLACEHOLDER = "2a96cbd8b46e442fc41c2b86b821562f.png"
+
 class GlideArtistFetcher(
     context: Context,
     mediaId: MediaId,
@@ -19,7 +23,11 @@ class GlideArtistFetcher(
     private val id = mediaId.resolveId
 
     override suspend fun execute(priority: Priority, callback: DataFetcher.DataCallback<in InputStream>): String {
-        return lastFmGateway.getArtist(id)!!.image
+        val image = lastFmGateway.getArtist(id)!!.image
+        if (image.endsWith(LAST_FM_PLACEHOLDER)) {
+            return ""
+        }
+        return image
     }
 
     override suspend fun shouldFetch(): Boolean {
