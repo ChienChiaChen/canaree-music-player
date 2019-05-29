52a347fefbcea216ac1fe1b42bf7120e2eea5f18
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-05-29 13:20:38 +0200

    dark mode behavior fixes on devices prior to Q

--- presentation-base/src/main/java/dev/olog/msc/presentation/base/theme/dark/mode/DarkMode.kt
+++ presentation-base/src/main/java/dev/olog/msc/presentation/base/theme/dark/mode/DarkMode.kt
@@ -3,6 +3,7 @@ package dev.olog.msc.presentation.base.theme.dark.mode
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.BuildCompat
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
@@ -10,6 +11,7 @@ import com.f2prateek.rx.preferences2.RxSharedPreferences
 import dev.olog.msc.core.dagger.qualifier.ApplicationContext
 import dev.olog.msc.core.dagger.qualifier.ProcessLifecycle
 import dev.olog.msc.presentation.base.R
+import dev.olog.msc.shared.utils.isQ
 import io.reactivex.BackpressureStrategy
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.collect
@@ -64,7 +66,13 @@ class DarkMode @Inject constructor(
 
     private fun onThemeChanged(theme: String) {
         val darkMode = when (theme) {
-            context.getString(R.string.prefs_dark_mode_2_value_follow_system) -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+            context.getString(R.string.prefs_dark_mode_2_value_follow_system) -> {
+                if (BuildCompat.isAtLeastQ() || isQ()){
+                    AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                } else {
+                    AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY
+                }
+            }
             context.getString(R.string.prefs_dark_mode_2_value_light) -> AppCompatDelegate.MODE_NIGHT_NO
             context.getString(R.string.prefs_dark_mode_2_value_dark) -> AppCompatDelegate.MODE_NIGHT_YES
             else -> throw IllegalStateException("invalid theme=$theme")
