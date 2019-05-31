245520b5298fc9f5e9fb3c3edb661f69d4cf0643
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-05-31 05:02:17 +0200

    deleted debug code

--- presentation-categories/src/main/java/dev/olog/msc/presentation/categories/track/CategoriesFragment.kt
+++ presentation-categories/src/main/java/dev/olog/msc/presentation/categories/track/CategoriesFragment.kt
@@ -2,13 +2,11 @@ package dev.olog.msc.presentation.categories.track
 
 import android.os.Bundle
 import android.view.View
-import androidx.appcompat.app.AppCompatDelegate
 import dev.olog.msc.core.MediaIdCategory
 import dev.olog.msc.core.gateway.prefs.AppPreferencesGateway
 import dev.olog.msc.presentation.base.FloatingWindowHelper
 import dev.olog.msc.presentation.base.extensions.act
 import dev.olog.msc.presentation.base.fragment.BaseFragment
-import dev.olog.msc.presentation.categories.BuildConfig
 import dev.olog.msc.presentation.categories.R
 import dev.olog.msc.presentation.categories.Tutorial
 import dev.olog.msc.presentation.categories.di.inject
@@ -46,22 +44,6 @@ class CategoriesFragment : BaseFragment() {
             view.pagerEmptyState.visibility = View.VISIBLE
         } else {
             view.pagerEmptyState.visibility = View.GONE
-        }
-
-        if (BuildConfig.DEBUG) { // TODO remove
-            view.header.setOnClickListener {
-                val current = AppCompatDelegate.getDefaultNightMode()
-                if (current == AppCompatDelegate.MODE_NIGHT_NO) {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                } else {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                }
-                activity?.recreate()
-            }
-            view.header.setOnLongClickListener {
-                //                DebugProbes.dumpCoroutines()
-                true
-            }
         }
     }
 
