86d04cfe66b69cfddfed79e97ea56a20846f808a
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-06-03 14:23:08 +0200

    fixes

--- presentation-base/src/main/java/dev/olog/msc/presentation/base/widgets/StatusBarView.kt
+++ presentation-base/src/main/java/dev/olog/msc/presentation/base/widgets/StatusBarView.kt
@@ -3,11 +3,8 @@ package dev.olog.msc.presentation.base.widgets
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
-import dev.olog.msc.shared.extensions.dimen
-import dev.olog.msc.shared.ui.R
+import android.view.WindowInsets
 import dev.olog.msc.shared.ui.extensions.colorSurface
-import dev.olog.msc.shared.ui.theme.immersive
-import dev.olog.msc.shared.utils.isP
 
 /**
  * Custom status bar to handleOnBackPressed device notch
@@ -19,21 +16,11 @@ class StatusBarView : View {
         init()
     }
 
+    private var viewHeight = 0
 
-    private val defaultStatusBarHeight = context.dimen(R.dimen.status_bar)
-    private val statusBarHeightPlusNotch by lazy {
-        if (isP()) {
-            rootWindowInsets.consumeStableInsets().stableInsetTop
-        } else {
-            TODO("VERSION.SDK_INT < M")
-        }
-    }
-    private val View.hasNotch by lazy {
-        if (isP()){
-            rootWindowInsets?.displayCutout != null
-        } else {
-            false
-        }
+    override fun onApplyWindowInsets(insets: WindowInsets?): WindowInsets {
+        viewHeight = insets?.systemWindowInsetTop ?: 0
+        return super.onApplyWindowInsets(insets)
     }
 
     private fun init() {
@@ -42,13 +29,7 @@ class StatusBarView : View {
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         if (!isInEditMode) {
-            val height = when {
-                context.immersive().isEnabled() -> 0
-                hasNotch -> statusBarHeightPlusNotch
-                else -> defaultStatusBarHeight
-            }
-
-            setMeasuredDimension(widthMeasureSpec, height)
+            setMeasuredDimension(widthMeasureSpec, viewHeight)
         } else {
             super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         }
